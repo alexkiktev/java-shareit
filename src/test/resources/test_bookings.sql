156f0ba774f4da4d09320782237ef71310d256ed
alexkiktev <agkiktev@yandex.ru>
2023-03-27 09:05:35 +0300

    ТЗ 15 третья итерация

--- src/test/resources/test_bookings.sql
+++ src/test/resources/test_bookings.sql
@@ -1,12 +1,8 @@
 insert into bookings (start_date, end_date, item_id, booker_id, status)
-values ((current_timestamp + (300 ||' minutes')::interval), (current_timestamp + (700 ||' minutes')::interval),
-        1, 3, 'APPROVED');
+values (timestampadd('hour', -10, now()), timestampadd('hour', -2, now()), 1, 3, 'APPROVED');
 insert into bookings (start_date, end_date, item_id, booker_id, status)
-values ((current_timestamp - (500 ||' minutes')::interval), (current_timestamp - (50 ||' minutes')::interval),
-        2, 1, 'APPROVED');
+values (timestampadd('hour', -6, now()), timestampadd('hour', -1, now()), 2, 1, 'APPROVED');
 insert into bookings (start_date, end_date, item_id, booker_id, status)
-values ((current_timestamp + (40 ||' minutes')::interval), (current_timestamp + (500 ||' minutes')::interval),
-        3, 1, 'APPROVED');
+values (timestampadd('hour', -4, now()), timestampadd('hour', 6, now()), 3, 1, 'APPROVED');
 insert into bookings (start_date, end_date, item_id, booker_id, status)
-values ((current_timestamp - (900 ||' minutes')::interval), (current_timestamp - (550 ||' minutes')::interval),
-        1, 2, 'APPROVED');
+values (timestampadd('hour', 3, now()), timestampadd('hour', 8, now()), 1, 2, 'APPROVED');
