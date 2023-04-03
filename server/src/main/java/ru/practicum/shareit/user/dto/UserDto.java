a08c35da3109df0a626b4acb12ac2586f3d14f84
alexkiktev <agkiktev@yandex.ru>
2023-04-03 06:56:47 +0300

    ТЗ 16 поправил валидацию на сервере

--- server/src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ server/src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -3,10 +3,6 @@ package ru.practicum.shareit.user.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import ru.practicum.shareit.utils.MarkerValidation;
-
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
 
 @Data
 @AllArgsConstructor
